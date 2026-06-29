ed4c71900b5addd2f43cbebe8096ef254b47b0fb
Manasi Mali <manasimali06@gmail.com>
2026-06-30 01:56:55 +0530

    Add files via upload

---
+++ Competative/RevNumbers.py
@@ -0,0 +1,17 @@
+# Write a program which accepts one number and prints that many numbers in reverse order.
+
+# Input: 5
+# Output: 5 4 3 2 1
+
+def Number(No):
+
+  for i in range(No, 0 ,-1):
+    print(i)
+
+def main():
+  Value = int(input("Enter number: "))
+
+  Number(Value)
+
+if __name__ == "__main__":
+  main()
